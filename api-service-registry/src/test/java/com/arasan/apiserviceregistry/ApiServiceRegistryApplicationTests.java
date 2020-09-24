426630ccaa14fd8b7a486eb15bec3e2a1d36e015
arasan21 <arasanbtech92@gmail.com>
2020-09-24 22:27:26 +0530

    initial commit

---
+++ api-service-registry/src/test/java/com/arasan/apiserviceregistry/ApiServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arasan.apiserviceregistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
