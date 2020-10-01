c7865c8824efc7052142f31199c0ae0eaac33bf2
arasan21 <arasanbtech92@gmail.com>
2020-10-01 22:11:52 +0530

    add authentication

---
+++ authservice/src/main/java/com/arasan/authservice/model/UserCredentials.java
@@ -0,0 +1,24 @@
+package com.arasan.authservice.model;
+
+public class UserCredentials {
+
+	private String username, password;
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+}
